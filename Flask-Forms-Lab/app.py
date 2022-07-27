2edf9a4459ec273c87894ea36f41b4905920e05c
sivanuz23-meet <109904738+sivanuz23-meet@users.noreply.github.com>
2022-07-27 17:07:42 +0300

    string: name still needs fixing

--- Flask-Forms-Lab/app.py
+++ Flask-Forms-Lab/app.py
@@ -1,6 +1,9 @@
 from flask import Flask, jsonify, request, render_template, redirect, url_for
 import random
 
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'super-secret-key'
+
 app = Flask(  # Create a flask app
 	__name__,
 	template_folder='templates',  # Name of html file folder
@@ -8,16 +11,31 @@ app = Flask(  # Create a flask app
 )
 
 
-username = "llo2ay"
+username = "123"
 password = "123"
-facebook_friends=["Loai","Yonathan","Adan", "George", "Fouad", "Celina"]
+facebook_friends=["Loai","Sivan","Adan", "George", "Fouad", "Celina"]
 
 
-@app.route('/')  # '/' for the default page
+@app.route('/', methods=['GET', 'POST'])  # '/' for the default page
 def login():
-  return render_template('login.html')
-  
+	if request.method == 'GET':
+		return render_template('login.html')
+	else:
+		if username == request.form['username'] and password == request.form['password']:
+			return redirect(url_for('home'))
+		else:
+			return render_template('login.html')
 
+@app.route('/home.html', methods=['GET', 'POST'])  # '/' for the default page
+def home():
+	return render_template('home.html', facebook_friends = facebook_friends)
+  
+@app.route('/friend_exists.html/<string:name>', methods=['GET', 'POST'])  # '/' for the default page
+def friends(name):
+	if name in facebook_friends:
+		return render_template('friend_exists.html', n = name, bool = True)
+	else:
+		return render_template('friend_exists.html', n = name, bool = False)
 
 
 if __name__ == "__main__":  # Makes sure this is the main process
